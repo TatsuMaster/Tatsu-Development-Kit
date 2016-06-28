04b7174e04cf7b12010c924b4ebcde806f03498a
Sebastian Langer <sevo@codepnp.net>
2016-06-28 15:57:25 +0200

    added more assertion macros which seems to be usefull

--- ttf/ttf.h
+++ ttf/ttf.h
@@ -6,6 +6,10 @@
 
 #include "../common/params.h"
 
+#ifndef NULL
+#define NULL 0
+#endif
+
 #ifndef true
 #define true 1
 #define TRUE true
@@ -34,7 +38,7 @@ static unsigned t_step_error_counter = 0;
 static clock_t begin;
 static clock_t end;
 
-#define _display_ts_name(ts_name) STORED_SCREEN_OUTPUT("  Test Step %d: \t%s...", t_step_counter, ts_name); print_result("[ .... ]\r", WARN);
+#define _display_ts_name(ts_name) STORED_SCREEN_OUTPUT("  Test Step %d:  %s...", t_step_counter, ts_name); print_result("[ .... ]\r", WARN);
 #define _eval_condition(condition) if (condition) { print_result("[ PASS ]\n", GOOD); ++t_step_pass_counter; } else { print_result("[ FAIL ]\n", ALERT); ++t_step_fail_counter; }
 
 #define TEST_SUITE_INIT() begin = clock(); prepare_console_info(); t_step_counter = 0; t_step_pass_counter = 0; t_step_fail_counter = 0; SCREEN_OUTPUT("Tatsu Test Framework - TDK Version %d.%d.%d\n", TDK_MAJOR_VERSION, TDK_MINOR_VERSION, TDK_PATCH_LEVEL);
@@ -47,5 +51,16 @@ static clock_t end;
 
 #define ASSERT_TRUE(ts_name, value) ASSERT_EQUALS(ts_name, value, true)
 #define ASSERT_FALSE(ts_name, value) ASSERT_EQUALS(ts_name, value, false)
+
+#define ASSERT_NULL(ts_name, pointer) ASSERT_EQUALS(ts_name, pointer, NULL)
+#define ASSERT_NOT_NULL(ts_name, pointer) ASSERT_EQUALS_NOT(ts_name, pointer, NULL)
+
+#define ASSERT_GREATER(ts_name, value, other) _display_ts_name(ts_name); ++t_step_counter; _eval_condition(value > other)
+#define ASSERT_LESS(ts_name, value, other) _display_ts_name(ts_name); ++t_step_counter; _eval_condition(value < other)
+
+#define ASSERT_GREATER_EQUAL(ts_name, value, other) _display_ts_name(ts_name); ++t_step_counter; _eval_condition(value >= other)
+#define ASSERT_LESS_EQUAL(ts_name, value, other) _display_ts_name(ts_name); ++t_step_counter; _eval_condition(value <= other)
+
+#define ASSERT_TYPE_SIZE(ts_name, type, size) ASSERT_EQUALS(ts_name, sizeof(type), size)
 
 #endif
